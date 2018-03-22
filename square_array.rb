2b0730472b0643a811d2a370b371b639a1846cab
Vanessa Toro <vanetoro84@gmail.com>
2018-03-22 18:16:15 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
-# def square_array(array)
-#   new_array= []
-#   array.each do |arr|
-#     new_array << arr**2
-#   end
-#   return new_array
-# end
+def square_array(array)
+  new_array= []
+  array.each do |arr|
+    new_array << arr**2
+  end
+  return new_array
+end
 
